1bd6be80b4dd5f1f2d919ad13a1c4c0aaa158196
Justin Tooley <jstooley823@gmail.com>
2017-12-21 23:04:55 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,11 +3,11 @@ class Song < ActiveRecord::Base
  	validates :released, inclusion: {in: [true, false]}
 
   with_options if: :released? do |song|
-         song.validates :release_year, presence: true
-         song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
   end 
- 
+
  	validates :artist_name, presence: true
 
- 	
+
 end
