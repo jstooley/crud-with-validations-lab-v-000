6c08981214d981f3ec73420ade387533ea0f458d
Justin Tooley <jstooley823@gmail.com>
2017-12-21 22:57:45 +0000

    automatically backed up by learn

--- app/models/songs.rb
+++ app/models/songs.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqiness: true
+  validates :title, uniqueness: true
   validates :released, inclusion: {in: (true false)}
+  validates :release_year,
 end
